2915b92c729da5f86480fcfeac412500c4724271
uherphilip-cmd <uher_philip@hotmail.com>
2026-07-11 14:35:35 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+'use strict';
+const CACHE='eberos-pwa-v1.0.0';
+const ASSETS=['./','./index.html','./eberos-charakter-builder.html','./manifest.webmanifest','./icons/eberos-icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response&&response.status===200&&response.type==='basic'){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match('./eberos-charakter-builder.html'))))});
