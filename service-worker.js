9a5ee9f942cbe402b20d49fcaefd8dc52a970d59
uherphilip-cmd <uher_philip@hotmail.com>
2026-07-12 08:49:03 +0200

    PWA-Cache auf Version 1.5.1 erhöhen

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,20 @@
 'use strict';
+const CACHE='eberos-pwa-v1.5.1';
+const OFFLINE='./eberos-charakter-builder.html';
+const ASSETS=['./','./index.html',OFFLINE,'./manifest.webmanifest','./icons/eberos-icon.svg','./icons/eberos-icon-180.png','./icons/eberos-icon-192.png','./icons/eberos-icon-512.png'];
+
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match(OFFLINE))));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response?.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
+});
+'use strict';
 const CACHE='eberos-pwa-v1.5.0';
 const OFFLINE='./eberos-charakter-builder.html';
 const ASSETS=['./','./index.html',OFFLINE,'./manifest.webmanifest','./icons/eberos-icon.svg','./icons/eberos-icon-180.png','./icons/eberos-icon-192.png','./icons/eberos-icon-512.png'];
